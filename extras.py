9fca414121f838c599df0918f410fac8798f19cf
Walter Bender <walter.bender@gmail.com>
2012-03-16 07:31:58 -0400

    Color Deducto strings

---
+++ extras.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#Copyright (c) 2012 Walter Bender
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# You should have received a copy of the GNU General Public License
+# along with this library; if not, write to the Free Software
+# Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
+
+
+from gettext import gettext as _
+
+'''
+Note: this file contains string from Color Deducto that are not
+included in the default packaaging of Deducto. The reason for
+gathering these strings here is to enable us to use one common POT
+file across both of these projects.
+'''
+
+COLORDEDUCTO = _('ColorDeducto')
