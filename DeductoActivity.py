sugarlabs/deducto

commit 656c9c8c1c4ad07b15831cdc2248e1a538b3d96d

Use format string with named arguments for xgettext
xgettext throws this error when it encounters a text that uses unnamed
arguments for string formating,

warning: 'msgid' format string with unnamed arguments cannot be properly localized:
                                 The translator cannot reorder the arguments.
                                 Please consider using a format string with named arguments,
                                 and a mapping instead of a tuple for the arguments.

use a format string with named arguments

Signed-off-by: Ibiam Chihurumnaya <ibiamchihurumnaya@gmail.com>

DeductoActivity.py
@@ -48,6 +48,9 @@ SERVICE = 'in.seeta.Deducto'
 IFACE = SERVICE
 PATH = '/in/seeta/DeductoActivity'
 
+BUTTON1 = '☑'
+BUTTON2 = '☒'
+
 
 class DeductoActivity(activity.Activity):
     """ Logic puzzle game """
@@ -249,8 +252,7 @@ class DeductoActivity(activity.Activity):
             self._false_button.set_tooltip(
                 _('Show a pattern that does not match the rule.'))
             self.status.set_label(
-                _('Explore patterns with the %s and %s buttons.') % (
-                    '☑', '☒'))
+                _(f'Explore patterns with the {BUTTON1} and {BUTTON2} buttons.')
             self._playing = False
         else:
             self._example_button.set_icon_name('example')

commit ffd1a7b1ff4cd1b4a4540653a7c8b319087e551c

Fix SyntaxError, regression introduced by 656c9c8
Reported-by: Walter Bender <walter@sugarlabs.org>
Signed-off-by: Ibiam Chihurumnaya <ibiamchihurumnaya@gmail.com>

DeductoActivity.py
@@ -48,8 +48,6 @@ SERVICE = 'in.seeta.Deducto'
 IFACE = SERVICE
 PATH = '/in/seeta/DeductoActivity'
 
-BUTTON1 = '☑'
-BUTTON2 = '☒'
 
 
 class DeductoActivity(activity.Activity):
@@ -251,8 +249,10 @@ class DeductoActivity(activity.Activity):
                 _('Show a pattern that matches the rule.'))
             self._false_button.set_tooltip(
                 _('Show a pattern that does not match the rule.'))
-            self.status.set_label(
-                _(f'Explore patterns with the {BUTTON1} and {BUTTON2} buttons.')
+            Button1 = '☑'
+            Button2 = '☒'
+            text = f"Explore patterns with the {Button1} and {Button2} buttons."
+            self.status.set_label(_(text))
             self._playing = False
         else:
             self._example_button.set_icon_name('example')